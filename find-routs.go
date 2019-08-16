d36d441ecb826ee560b67afbd7a58cc703f03b1d
Mehdi Mirabian <mehdi@arcules.com>
2019-08-16 11:11:28 -0700

    new plan

--- find-routs.go
+++ find-routs.go
@@ -9,6 +9,10 @@ import (
 	"strings"
 )
 
+//TODO: start with the way you are doing and adding roots for the ones that have no parent
+//Then for everything else run dfs and if dfs returns false add it to the subdomains
+//this way you will have a tree per root
+
 type Routs struct {
 	Domains []*Domain
 }
@@ -19,28 +23,36 @@ type Domain struct {
 }
 
 var (
-	nodeTable = map[string]*Domain{}
-	root      []*Domain
+	nodeTable   = map[string]*Domain{}
+	existingSub = map[string][]string{}
+	root        []*Domain
 )
 
 var fileName string
 
 func add(name, parentId string) {
 	fmt.Printf("add:name=%v parentId=%v\n", name, parentId)
-
+	preExisting := false
 	node := &Domain{DomainName: name, SubDomains: []*Domain{}}
 
 	if parentId == "" && nodeTable[name] == nil {
 		root = append(root, node)
 	} else {
-
 		parent, ok := nodeTable[parentId]
 		if !ok {
 			fmt.Printf("add: parentId=%v: not found\n", parentId)
 			return
 		}
-
-		parent.SubDomains = append(parent.SubDomains, node)
+		for _, dom := range existingSub[parent.DomainName] {
+			if node.DomainName == dom {
+				preExisting = true
+				break
+			}
+		}
+		if !preExisting {
+			parent.SubDomains = append(parent.SubDomains, node)
+			existingSub[parent.DomainName] = append(existingSub[parent.DomainName], node.DomainName)
+		}
 	}
 
 	nodeTable[name] = node
@@ -76,7 +88,7 @@ func main() {
 		log.Fatal("error while checking environment: ", err.Error())
 	}
 	fmt.Printf("main: reading input from stdin\n")
-	scan()
+	loadConfig()
 	fmt.Printf("main: reading input from stdin -- done\n")
 	show()
 	fmt.Printf("main: end\n")
@@ -95,33 +107,7 @@ func checkEnv() error {
 	return nil
 }
 
-//
-//func loadConfig() (*Routs, error) {
-//	//Read the routing config file
-//	file, err := os.Open(fileName)
-//	if err != nil {
-//		log.Fatalln("error while reading the file: ", err.Error())
-//	}
-//	scanner := bufio.NewScanner(file)
-//	scanner.Split(bufio.ScanLines)
-//	var txtlines []string
-//
-//	for scanner.Scan() {
-//		txtlines = append(txtlines, scanner.Text())
-//		line := strings.Split(scanner.Text(), ".")
-//		for _, dom := range(line) {
-//			if myMap[dom] != nil {
-//				myMap = myMap[dom].Subset
-//			}
-//			}
-//		}
-//	}
-//	log.Println(txtlines)
-//	//Do the following
-//	return nil, nil
-//}
-
-func scan() {
+func loadConfig() {
 	file, err := os.Open(fileName)
 	if err != nil {
 		log.Fatalln("error while reading the file: ", err.Error())
